ffe09073ed30e592cfa1b955c07918abbc9f544f
matheusmoraesporto99@gmail.com <matheusmoraesporto99@gmail.com>
2021-07-13 23:00:06 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import Box from '../src/components/Box';
 import MainGrid from '../src/components/MainGrid';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 // Usamos o import com as chaves, quando o arquivo não 'export default', ou seja exporta mais de uma função.
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 
 function ProfileSidebar(props) {
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
@@ -17,9 +28,30 @@ export default function Home() {
 
   const pessoasFavoritas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho'];
 
+  const [comunidades, setComunidades] = React.useState([{
+    id: '11365344834512',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
+
+  const handleCriaComunidade = (event) => {
+    event.preventDefault();
+
+    // Obtém todos os dados do formulário
+    const formData = new FormData(event.target);
+
+    const comunidade = {
+      id: new Date().toISOString(),
+      title: formData.get('title'),
+      image: formData.get('image')
+    };
+
+    setComunidades([...comunidades, comunidade]);
+  };
+
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={githubUser} />
       <MainGrid>
         <div className="profileArea" style={{ gridArea: 'profileArea' }}>
           <ProfileSidebar githubUser={githubUser} />
@@ -31,9 +63,50 @@ export default function Home() {
 
             <OrkutNostalgicIconSet />
           </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+
+            <form onSubmit={handleCriaComunidade}>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Coloque uma url para usar de capa"
+                  name="image"
+                  aria-label="Coloque uma url para usar de capa"
+                />
+              </div>
+
+              <button>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
 
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Comunidades ({comunidades.length})</h2>
+            <ul>
+              {comunidades.map((c) => {
+                return (
+                  <li key={c.id}>
+                    <a href={`/users/${c.title}`}>
+                      <img src={c.image} />
+                      <span>{c.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
@@ -52,9 +125,6 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          {/* <Box>
-            Comunidades
-          </Box> */}
         </div>
       </MainGrid>
     </>
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -57,4 +57,4 @@ const Box = styled.div`
   }
 `;
 
-export default Box;
+export default Box
